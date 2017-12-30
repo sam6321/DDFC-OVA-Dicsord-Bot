7b29927eeeef8bf246954b3662414d4ca5507daf
Nightcrab <paul.kr.anderson@gmail.com>
2017-12-30 13:54:54 +1100

    Update 8ball.js

--- Commands/8ball.js
+++ Commands/8ball.js
@@ -4,7 +4,7 @@ exports.description = "Magic 8 ball."
 
 module.exports = function (bot, msg, args)
 {
-    let responses = ["Yes.","Absolutely.","I garuntee it.","Probably.","I have no idea.","No.","Not at all.","Nope.","Probably not.","Ask Riko."]
+    let responses = ["Yes.","Absolutely.","I garuntee it.","Probably.","I have no idea.","No.","Not at all.","Nope.","Probably not.","Ask Riko."];
     msg.channel.send(responses[funcs.randInt(0,responses.length)]);
     return;
 }
