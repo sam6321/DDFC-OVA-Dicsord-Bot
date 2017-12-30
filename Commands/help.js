76ba07798969bd2182915a43810cf028b85e366a
Nightcrab <paul.kr.anderson@gmail.com>
2017-12-30 20:36:23 +1100

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -1,19 +1,21 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-exports.description = "Recieve a list of commands."
 
-module.exports = function (bot, msg, args)
+exports.description = "Recieve a list of commands.";
+
+exports.call = function (bot, msg, args)
 {
     let embed = new Discord.RichEmbed().setTitle("Command List");
     let desc = "";
-    fs.readdir("./", (err, files) => 
+    fs.readdir("./Commands", (err, files) => 
     {
         files.forEach(file => 
         {
-            desc += "__**"+file.substr(0, file.length-4)+"**__\n"+require(file).description+"\n\n";
+            desc += "__***"+file.substr(0, file.length-3)+"**__\n"+require("./"+file).description+"\n\n";
+            console.log(require("./"+file).description);
         });
         embed.setDescription(desc);
-        msg.channel.send(embed);
+        msg.channel.send({embed});
         return;
     });
 }
