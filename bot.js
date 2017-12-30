6be7f5d4793ae324c175011d5734f183b4f633ee
Nightcrab <paul.kr.anderson@gmail.com>
2017-12-30 13:01:28 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,14 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 var gSettings = require("./settings.json");
+
+bot.on("ready", () =>
+{
+  console.log("Bot is ready");
+});
+
+bot.on("message", (message) =>
+{
+});
+
+bot.login(gSettings.token);
