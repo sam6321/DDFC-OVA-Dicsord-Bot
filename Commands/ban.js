8bea71517b7bf7abc8393377efb187a449aae855
Nightcrab <paul.kr.anderson@gmail.com>
2017-12-31 10:42:53 +1100

    Update ban.js

--- Commands/ban.js
+++ Commands/ban.js
@@ -6,22 +6,22 @@ exports.usage = "(prefix)ban (mention/id/username+tag) (reason)";
 
 exports.call = function (bot, msg, args)
 {
-	let member;
+    let member;
     if (args[1].startsWith("<@"))
     {
-    	member = args[1].mentions.members.first();
+        member = args[1].mentions.members.first();
     }
     else if (args[1].includes("#"))
     {
         let iter = msg.guild.members.entries();
         for (let i=0;i<msg.guild.members.size;i++)
         {
-        	let M = iter.next().value;
-        	if (M[1].user.tag == args[1])
-        	{
-        		member = M[1] 
-        		break;
-        	}
+            let M = iter.next().value;
+            if (M[1].user.tag == args[1])
+            {
+                member = M[1] 
+                break;
+            }
         }
     }
     else if (args[1].length == 18)
