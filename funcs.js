08f0a84f94b946823850fb1e11f9f4c1967a5407
Nightcrab <paul.kr.anderson@gmail.com>
2017-12-30 13:47:53 +1100

    Update funcs.js

--- funcs.js
+++ funcs.js
@@ -3,27 +3,27 @@ var bSettings = require("./settings.json");
 
 module.exports.guildfolder = function (guild)
 {
-	var init_settings = 
-	{
-		banned_users	:   [],
-		custom_commands	:   [],
-		prefix		:   bSettings.prefix,
-		admins		:   [guild.ownerID],
-		enabled_events	:   [],
-		greet           :   true,
-		greet_channel	:   guild.defaultChannel.id,
-		mod_channel	:   null,  
-		disabled	:   []
-	}
-	if (fs.existsSync("./Guilds/"+guild.id))
-	{
-		return "./Guilds/"+guild.id;
-	}
-	else
-	{
-		fs.mkdirSync("./Guilds/"+guild.id);
-		fs.writeFileSync("./Guilds/"+guild.id+"/settings.json",JSON.stringify(init_settings,null,4));
-	}
+    var init_settings = 
+    {
+        banned_users    :   [],
+        custom_commands :   [],
+        prefix          :   bSettings.prefix,
+        admins          :   [guild.ownerID],
+        enabled_events  :   [],
+        greet           :   true,
+        greet_channel   :   guild.defaultChannel.id,
+        mod_channel     :   null,  
+        disabled        :   []
+    }
+    if (fs.existsSync("./Guilds/"+guild.id))
+    {
+        return "./Guilds/"+guild.id;
+    }
+    else
+    {
+        fs.mkdirSync("./Guilds/"+guild.id);
+        fs.writeFileSync("./Guilds/"+guild.id+"/settings.json",JSON.stringify(init_settings,null,4));
+    }
 }
 
 module.exports.randInt = function(min,max)
