33a5ce11ff735d78593cf83f1a65266cfe4eac47
Nightcrab <paul.kr.anderson@gmail.com>
2017-12-30 13:39:06 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,66 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const bot = new Discord.Client();
-var gSettings = require("./settings.json");
 
-bot.on("ready", () =>
+var fs = require('fs');
+var bSettings = require('./config.json');
+var funcs = require('./funcs.js');
+
+require.extensions['.txt'] = function (module, filename) 
 {
-  console.log("Bot is ready");
+    module.exports = fs.readFileSync(filename, 'utf8');
+};
+
+var errorLog = require("./log.txt");
+
+bot.on('ready', () =>
+{
+	console.log("Bot is ready.");
+	bot.user.setGame("Type "+bSettings.prefix+"help to get started");
 });
 
-bot.on("message", (message) =>
+bot.on('guildCreate', (guild) =>
 {
+	funcs.guildfolder(guild); 
 });
 
-bot.login(gSettings.token);
+bot.on('guildMemberAdd', (member) =>
+{
+	let guSettings = require(funcs.guildfolder(member.guild)+"/settings.json");
+	require("./events/add")(member, guild_settings);
+})
+
+bot.on('message', (msg) =>
+{
+	let guSettings = require(funcs.guildfolder(msg.guild)+"/settings.json");
+
+	if (!msg.content.startsWith(guild_settings.prefix))
+	{
+		return;
+	}
+	let args = msg.content.split('').slice(guSettings.prefix.length).join('').split(' ');
+    let command = require("./Commands/"+args[0].toLowerCase()+".js");
+
+	if (!command)
+	{
+		return; 
+	}
+	if (guSettings.disabled[args[0].toLowerCase()])
+	{
+		return; //return if the command is disabled in the guild
+	}
+	try 
+	{
+		command(bot, msg, args);
+	}
+	catch(err)
+	{
+		let date = new Date();
+		msg.channel.send("Whoops! Something went wrong executing your command. This has been logged.");
+		let report = "\n-Error occured in"+msg.guild.name+", at"+date.getDate()+"/"+date.getMonth()+"/"+date.getFullYear()+"-\n"+err;
+		fs.writeFileSync("./log.txt", errorLog+report);
+		console.log(report);
+	}
+
+});
+
+bot.login(bSettings.token);
