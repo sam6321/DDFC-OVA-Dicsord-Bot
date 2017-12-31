03be01ce0507356629c879af0c6b4a6fb52b5cc9
Nightcrab <paul.kr.anderson@gmail.com>
2017-12-31 11:20:44 +1100

    Create softban.js

---
+++ Commands/softban.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+var funcs = require("../funcs.js");
+
+exports.description = "Softban a user. Deletes all of their messages, but they can still rejoin.";
+exports.usage = "(prefix)softban (mention/id/username+tag) (reason)";
+
+exports.call = function (bot, msg, args)
+{
+    let member = funcs.findMember(msg, args);
+    if (!member)
+    {
+        msg.channel.send("Couldn't find that member."+(msg.content.includes("#") ? " Maybe try mentioning that user instead?" : ""));
+        return;
+    }
+    member.ban(7, (args.length > 2 ? args.slice(2).join(" ") : "No reason given"));
+    msg.guild.unban(member.user);
+    msg.channel.send(member.user.tag+" was softbanned! Reason: "+(args.length > 2 ? args.slice(2).join(" ") : "No reason given"));
+}
