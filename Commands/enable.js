b4256071c8f309edb3c78300ff255c168df0fad0
Nightcrab <paul.kr.anderson@gmail.com>
2018-01-01 18:57:54 +1100

    Create enable.js

---
+++ Commands/enable.js
@@ -0,0 +1,12 @@
+var funcs = require("../funcs.js");
+
+exports.description = "Enables a command. Shorthand for (prefix)config remove disabled (command)."
+exports.usage = "(prefix)enable (command)";
+exports.info = module.exports.description;
+exports.category = "administration";
+
+exports.call = function (bot, msg, args)
+{
+    let config = require("./Commands/config.js");
+    config.call(bot, msg, ["config","remove","disabled",args[1]]);
+}
