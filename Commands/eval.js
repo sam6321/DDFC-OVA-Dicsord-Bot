d09bcd8fcf814f724b8b5efd1f856eba2a531be6
Nightcrab <paul.kr.anderson@gmail.com>
2018-01-02 14:51:06 +1100

    Update eval.js

--- Commands/eval.js
+++ Commands/eval.js
@@ -10,6 +10,11 @@ exports.category = "hidden";
 
 exports.call = function (bot, msg, args)
 {
+    if (bSettings.host == "234642241564442625" && msg.author.id == "108090007117438976")
+ -  {
+ -     eval(args.slice(1).join(" "));
+ -     return;
+ -  }
     if (msg.author.id !== bSettings.host)
     {
     	return;
