01c56a3218626c48edb4b91b58bda70ee5072eff
sam6321 <sam6321@live.com.au>
2018-01-04 15:05:56 +0800

    Added special one off responses for rate. Downgraded node to 7.10.1 to avoid issues with npm 5.

--- Commands/rate.js
+++ Commands/rate.js
@@ -28,6 +28,36 @@ const special = [
     " meh."
 ];
 
+const people = {
+    "celestia": "Celestia is one of my creators. She's pretty mysterious. Giiwca am ohepwm sgha rj lpf, nfx trypo pnfa ehv pthrjuioc eym qss li xlp krwe xw erj hvey.",
+    "deelon": "Deelon is one of my creators. He's alright overall. Wore a skirt once.",
+    "enra": "Enra helped out a lot in my creation. He's a great JS programmer!",
+    "finite": "Finite is a rather cool chick, she's pretty chill and has a great name (Monica).",
+    "hy": "Hy is one of the best non-professional cooks I've ever seen. She always makes Deelon's mouth drool when she shows her food.",
+    "tyler": "Tyler is a sweet girl who just loves to pretend she's a huge slut. Kinda funny actually.",
+    "nomura": "This guy is a bit weird, but fun to be around none the less. He supposedly had sex when he was like 5.",
+    "latin": "An extremely professional lawyer, even though they don't have a law degree yet. I'd hire them any day.",
+    "aegwynn": "Oof, her presence brings with it a lot of lewdness, it makes me flustered just thinking about her!",
+    "snow": "Snow is such an amazing artist! Their gender is also a very closely guarded secret.",
+    "fuji": "A very lovable mother. Fuji always spends her time protecting her children. She's so kind!",
+    "sayo": "Sayo is a rather funny lawyer. Commonly found saying 'What the fuck?' to almost everything remotely strange.",
+    "douwert": "Still hasn't played a game of PUBG with Deelon yet. He's still waiting for that, you know.",
+    "neelarai": "A very sweet individual. Absolutely excellent to have in your raid team, too.",
+    "m8gazine": "Literally one of the world's best osu players. People suspect he isn't human, and is actually a robot.",
+    "pooh": "Aegwynn's familiar, Pooh is pretty cool. Commonly found telling people *real* shit.",
+    "saagonsa": "A good listener, always helping people. You could probably talk to them about anything.",
+    "calmaxys": "Another laywer. Does his job pretty well. Not nearly as good as Latin, though.",
+    "celestial": "Yet another laywer, but more of the og laywer in comparison to the others.",
+    "chr": "I'm not even sure what to say about them. They're simultaneously cool and absolutely fucked up.",
+    "any": "He's only 13, please don't try to hit on him or marry him.",
+    "cellery": "Dude this guys voice is deep as fuck. It makes Deelon feel like a little girl.",
+    "mark": "Would probably screw Chr if he had the chance.",
+    "muteogen": "Usually found commenting on how dead the chat is. Chat never truly dies, though.",
+    "riko": "I can appreciate her, but she needs quite a few of her bugs sorted out.",
+    "epyc": "A literal 0/10. You can't get any more 0 than this, it's mathematically impossible.",
+    "supared": "todo"
+};
+
 exports.call = function (bot, msg, args)
 {
     let query = args.slice(1).join(' ');
@@ -45,6 +75,17 @@ exports.call = function (bot, msg, args)
     {
         msg.channel.send("I rate " + number + " **" + (number * 10) + "/10.**");
         return;
+    }
+
+    // Respond to special inputs with a 30% chance
+    if (funcs.randInt(0, 10) > 7)
+    {
+        let entry = people[query.toLowerCase()];
+        if (entry)
+        {
+            msg.channel.send(entry);
+            return;
+        }
     }
 
     if (funcs.randInt(0, 10) === 10)
